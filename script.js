093e369726030b7ab7f8adafbca6677f044a7403
dhiyaneshsiva <dhiyanesh.siva@outlook.com>
2023-08-15 14:00:32 +0530

    Project-Added

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Freelancer","Fullstack Mern Developer","Web Designer","Web Developer"],
+        strings: ["Freelancer","Fullstack Mern Developer","Web Designer","Web Developer","Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Freelancer","Fullstack Mern Developer","Web Designer","Web Developer"],
+        strings: ["Freelancer","Fullstack Mern Developer","Web Designer","Web Developer","Software Developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
